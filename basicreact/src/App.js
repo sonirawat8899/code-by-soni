2f3969e07ecbd504017c2d3f33d47ad2b69b324a
soni rawat <sonirawat4968@gmail.com>
2025-04-15 22:43:08 +0530

    add

---
+++ basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+  <h1>Hey , I am Soni</h1>
+  );
+}
+
+export default App;
---
+++ vite-react/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+     <h1>Hey , I'm Sona</h1>
+    </>
+  )
+}
+
+export default App
